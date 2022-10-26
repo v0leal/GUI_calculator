7096fbe22e1c14a1483f989756a3cb7784d8f41b
Leonid V <leovorobey74@gmail.com>
2022-10-26 16:34:17 +0500

    Bugs fixed, app is ready for debugging

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import math
     # FUNCTIONS
 
 def press_key(event):
@@ -10,9 +9,10 @@ def press_key(event):
     elif event.char == '\r':
         calculate()
     else:
-        entry.delete(-1,-1)
+        entry.delete(-1, -1)
 
 def add_digit(digit):
+    entry['state'] = 'normal'
     value = entry.get()                     # Add digit to the entry row
     if value == 'Dividing by zero':
         entry.delete(0, 'end')                                  # If 'yes', delete the last operation symbol
@@ -22,41 +22,47 @@ def add_digit(digit):
         entry.insert('end', value + digit)                              # Insert digit
         if value[0] == '0' and len(value) == 1:                               # Check if the first digit '0'
             entry.delete(0, 1)                                  # If 'yes', delete the first '0', if not - pass
+    entry['state'] = 'readonly'
 
 def add_digit_button(digit):                                # Add digit button
-    return tk.Button(main, text=digit, bd=3,
-                     command=lambda: add_digit(digit))
+    return tk.Button(main, text=digit, bd=5, font=('Arial', 15, 'bold'), fg='white',
+                     command=lambda: add_digit(digit), bg='blue')
 
 def add_operation_button(operation):                        # Add operation (+-*/) buttons
-    return tk.Button(main, text=operation, bd=3,
+    return tk.Button(main, text=operation, bd=3, font=('Arial', 15, 'bold'),
                      command=lambda: operation_button(operation))
 
 def operation_button(operation):                            # Insert operation to entry row
+    entry['state'] = 'normal'
     value = entry.get()                                     # Save current entry row to a temporary variable
     if value == 'Dividing by zero':
         entry.delete(0, 'end')                                  # If 'yes', delete the last operation symbol
         entry.insert('end', '0')
     else:               # Delete current entry row
-        if value[-1] in '+-/*' and len(value) == 1:                                 # Check if the last entry symbol is already operation mark
+        if value[-1] in '+-/*':                                 # Check if the last entry symbol is already operation mark
             value = value[:-1]
-        elif '+' in value or '-' in value or '*' in value or '/' in value:
-            calculate()
-            value = entry.get()
+            if '+' in value[:-1] or '-' in value[:-1] or '*' in value[:-1] or '/' in value[:-1]:
+                calculate()
+                value = entry.get()
         entry.delete(0, 'end')                                  # If 'yes', delete the last operation symbol
         entry.insert('end', value + str(operation))             # Add previous value of the entry row and add the new operation symbol
+    entry['state'] = 'readonly'
 
 def add_clear_button():
-    return tk.Button(main, text='C', bd=3,
+    return tk.Button(main, text='C', bd=3, font=('Arial', 15, 'bold'),
                      command=lambda: clear_button())
 
 def clear_button():
+    entry['state'] = 'normal'
     entry.delete(0, 'end')
     entry.insert('end', '0')
+    entry['state'] = 'readonly'
 
 def add_calculate_button():
-    return tk.Button(main, text='=', bd=3,
+    return tk.Button(main, text='=', bd=3, font=('Arial', 15, 'bold'),
                      command=lambda: calculate())
 def calculate():
+    entry['state'] = 'normal'
     try:
         if entry.get()[-1] in '+-*/':
             ans = entry.get() + entry.get()[:-1]
@@ -71,8 +77,10 @@ def calculate():
         ans = 'Dividing by zero'
         entry.delete(0, 'end')
         entry.insert('end', ans)
+    entry['state'] = 'readonly'
 
 def delete_symb(event):
+    entry['state'] = 'normal'
     value = entry.get()
     if len(value) == 1:
         entry.delete(0, 'end')
@@ -80,6 +88,7 @@ def delete_symb(event):
     elif len(value[:-1]) > 0:
         entry.delete(0,'end')
         entry.insert('end', value[:-1])
+    entry['state'] = 'readonly'
 
     # INITIALISATION
 
@@ -90,10 +99,9 @@ icon = tk.PhotoImage(file='icon.png')                        # Save icon of the 
 main.iconphoto(False, icon)                                  # Set icon of the main window
 main.config(bg="#A6EBF2")                                    # Set default background color
 main.title("L.V.'s GUI calculator")                          # Rename main window's title
-main.geometry("300x400+200+250")                             # Set main window's default size (300px. x 400px.) and position (200px. from left border, 250px. from up border)
-main.resizable(True, True)                                   # Property of resize-ability of main window
-main.minsize(300, 400)                                       # Set minimum size of the main window
-main.maxsize(500, 600)                                       # Set maximum size of the main window
+main.geometry("300x440+200+250")                             # Set main window's default size (300px. x 400px.) and position (200px. from left border, 250px. from up border)
+main.resizable(False, False)                                   # Property of resize-ability of main window
+main.minsize(300, 440)                                       # Set minimum size of the main window
 
 main.bind('<Key>', press_key)
 #main.bind('<Enter>', calculate)
@@ -101,37 +109,38 @@ main.bind('<BackSpace>', delete_symb)
     # WORKBENCH CUSTOMIZATION
 
         ## Rows
-main.grid_rowconfigure(0, minsize=30)
-main.grid_rowconfigure(1, minsize=30)
-main.grid_rowconfigure(2, minsize=40)
-main.grid_rowconfigure(3, minsize=40)
-main.grid_rowconfigure(4, minsize=40)
-main.grid_rowconfigure(5, minsize=40)
+main.grid_rowconfigure(0, minsize=main.winfo_height()/6)
+main.grid_rowconfigure(1, minsize=main.winfo_height()/6)
+main.grid_rowconfigure(2, minsize=main.winfo_height()/6)
+main.grid_rowconfigure(3, minsize=main.winfo_height()/6)
+main.grid_rowconfigure(4, minsize=main.winfo_height()/6)
+main.grid_rowconfigure(5, minsize=main.winfo_height()/6)
 
         ## Columns
-main.grid_columnconfigure(0, minsize=50)
-main.grid_columnconfigure(1, minsize=50)
-main.grid_columnconfigure(2, minsize=50)
-main.grid_columnconfigure(3, minsize=50)
+main.grid_columnconfigure(0, minsize=main.winfo_width()/4)
+main.grid_columnconfigure(1, minsize=main.winfo_width()/4)
+main.grid_columnconfigure(2, minsize=main.winfo_width()/4)
+main.grid_columnconfigure(3, minsize=main.winfo_width()/4)
 
 
     # ELEMENTS CREATING AND POSITION
         ## Label name
-lable_name = tk.Label(main, text='L.V.\'s\nGUI calculator',  # Add text into 'main' window
-                      bg='#999999',                          # Background color
-                      fg='yellow',                           # Text color
-                      width=15,                              # Set width of labelbox
-                      height=2,                              # Set height of labelbox
+label_name = tk.Label(main, text='L.V.\'s\ncalculator',  # Add text into 'main' window
+                      bg='#A6EBF2',                          # Background color
+                      #fg='yellow',                           # Text color
+                      #padx=15,                              # Set width of labelbox
+                      #pady=2,                              # Set height of labelbox
                       font=('Times New Roman', 20, 'bold'),  # Set font, size, type
-                      relief=tk.RAISED,                      # Create frame
-                      bd=5                                   # Set frame's width
+                      #relief=tk.RAISED,                      # Create frame
+                      #bd=5,                                   # Set frame's width
                       )
-lable_name.grid(row=0, column=0, columnspan=3)               # Locate lable name
+label_name.grid(row=0, column=0, columnspan=4)               # Locate lable name
 
         ## Entry row
 entry = tk.Entry(main, font=('Times New Roman', 20), justify=tk.RIGHT)  # Create entry row
 entry.insert(0, '0')                                                    # Insert default value
-entry.grid(row=1, column=0, columnspan=4, pady=20)                      # Locate entry row
+entry.grid(row=1, column=0, columnspan=4, pady=20)
+entry['state'] = 'readonly'                                         # Locate entry row
 
         ## Digit buttons
 btn1 = add_digit_button('1').grid(row=2, column=0, stick='ewns', padx=2, pady=2)
